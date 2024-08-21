bb172ab8234977d8b7285d63fbb8b83c23d0ff77
owen griere <owen.griere@etu.u-paris.fr>
2024-08-21 16:49:55 +0200

    preentation

--- PROCESS.sh
+++ PROCESS.sh
@@ -12,14 +12,14 @@ mkdir -p $path/RESULT
 
 nextflow run $path/tools/PROCESS.nf -c $configNF -with-report report.html ## nextflow parallelization by chromosome
 
-printf "- Results Concatenation and Manhattan Plot ..."
+printf "Results Concatenation and Manhattan Plot ..."
 awk '(NR == 1) || (FNR > 1)' $path/RESULT/*.csv > $path/df_analyse.csv ## concatenate all the csv in one
 rm $path/RESULT/*.csv
 mv $path/df_analyse.csv $path/RESULT/
 python $path/tools/Plot.py $path/RESULT/df_analyse.csv $path/RESULT ## Plot Manhattan Plot
 echo -e "DONE\n"
 
-printf "- FILTERING MORFEE FILE ..." 
+printf "FILTERING MORFEE FILE ..." 
 python $path/tools/FilterXLSX.py $MORFEE_tab $path/variants.txt $path/RESULT
 echo -e "DONE\n"
 
